4080ccb080b5548d7253e14af98989c95ba6b008
Brandon Buxin <bubr@sbim.be>
2019-07-08 14:32:54 +0200

    Add delete and put example methods

--- Controllers/ExampleController.cs
+++ Controllers/ExampleController.cs
@@ -7,13 +7,13 @@ using ExpensesManagingApi.Models;
 
 namespace TodoApi.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/examples")]
     [ApiController]
-    public class TodoController : ControllerBase
+    public class ExampleController : ControllerBase
     {
         private readonly ExampleContext _context;
 
-        public TodoController(ExampleContext context)
+        public ExampleController(ExampleContext context)
         {
             _context = context;
 
@@ -24,6 +24,57 @@ namespace TodoApi.Controllers
                 _context.ExampleItems.Add(new ExampleItem { name = "Item1" });
                 _context.SaveChanges();
             }
+        }
+        // GET: api/examples/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ExampleItem>> GetExampleItem(long id)
+        {
+            var ExampleItem = await _context.ExampleItems.FindAsync(id);
+
+            if (ExampleItem == null)
+            {
+                return NotFound();
+            }
+
+            return ExampleItem;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ExampleItem>> PostExampleItem(ExampleItem item)
+        {
+            _context.ExampleItems.Add(item);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetExampleItem), new { id = item.Id }, item);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutExampleItem(long id, ExampleItem item)
+        {
+            if (id != item.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(item).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteExampleItem(long id)
+        {
+            var ExampleItem = await _context.ExampleItems.FindAsync(id);
+
+            if (ExampleItem == null)
+            {
+                return NotFound();
+            }
+
+            _context.ExampleItems.Remove(ExampleItem);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
         }
     }
 }
