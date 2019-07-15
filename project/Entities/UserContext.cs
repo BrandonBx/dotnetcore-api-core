a810388c19c5c1bb5a4152e2cc50313e959d6b39
Brandon Buxin <bubr@sbim.be>
2019-07-15 11:49:38 +0200

    Firstly, I don't know what happened with my previous commit. Then it seems that MySQL doesn't support EF Core 2.2 so let's try with Pomelo

--- project/Entities/UserContext.cs
+++ project/Entities/UserContext.cs
@@ -1,5 +1,4 @@
 using ExpensesManaging.project.POCO;
-using JetBrains.Annotations;
 using Microsoft.EntityFrameworkCore;
 
 namespace ExpensesManaging.project.Entities
@@ -7,13 +6,9 @@ namespace ExpensesManaging.project.Entities
     public class UserContext : DbContext
     {
         public DbSet<User> Users { get; set; }
-        protected UserContext(DbContextOptions<UserContext> options) : base(options)
+        public UserContext(DbContextOptions<UserContext> options) : base(options)
         {
 
-        }
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
         }
     }
 } 
--- project/Models/User.cs
+++ project/Models/User.cs
@@ -1,13 +1,21 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace ExpensesManaging.project.POCO
 {
     public class User
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
         public string Username { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
         public string Email { get; set; }
-        public int age { get; set; }
         public DateTime BirthdayDate { get; set; }
     }
 }
